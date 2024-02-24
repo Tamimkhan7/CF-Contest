80cd2db07aac3dcd3cbabf9d4bd13389780dcec4
Tamim khan <mdtamimkhan842@gmail.com>
2024-02-25 02:12:25 +0600

    today

--- A Desorting.cpp
+++ A Desorting.cpp
@@ -1,51 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-#define faster                    \
+#define MTK                       \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
-#define sq(x) (x) * (x)
-#define PI acos(-1.0)
+typedef long long int ll;
 #define all(x) x.begin(), x.end()
-#define nl '\n'
 #define mod 1000000007
-int main()
+void solve()
 {
-    faster;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            cout << 0 << '\n';
+            return;
+        }
+    }
+    int ans = 1e9;
+    for (int i = 0; i < n - 1; i++)
+        ans = min(abs(a[i] - a[i + 1]), ans);
+    cout << ans / 2 + 1 << '\n';
+}
+int32_t main()
+{
+    MTK;
     int t;
     cin >> t;
     while (t--)
-    {
-        int n;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        int c = 0, k = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] != arr[i + 1])
-            {
-                k++;
-                break;
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] <= arr[i + 1])
-            {
-                c++;
-                swap(arr[i], arr[i + 1]);
-            }
-        }
-        if (k != 0)
-            cout << 0 << endl;
-        else
-            cout << c << endl;
-    }
+        solve();
 }
