7f23b52d0af28dd11453362d2cdaef8b7d7d95e1
Tamim khan <mdtamimkhan842@gmail.com>
2024-05-30 16:56:48 +0600

    modified round 945(div 2) and modifed round 945(div2)

--- round 945 (div 2)/A.cpp
+++ round 945 (div 2)/A.cpp
@@ -5,7 +5,7 @@ using namespace std;
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
-    #define all(x) (x).begin(), (x).end()
+#define all(x) (x).begin(), (x).end()
 #define ll int long long
 void solve()
 {
